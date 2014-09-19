acdbe4e7c92c7f87d5edf520031c334793ebd294
Justin Basinger <justin.m.basinger@gmail.com>
2014-09-18 20:06:41 -0400

    Testing
    added a file for testing

---
+++ testing.py
@@ -0,0 +1,13 @@
+dictionary={'key':'value','Another Key':'Some Other Value', 'key':'oubiyg'}
+dictionary['A Third Key']='Another Value!!'
+#print dictionary
+dictionary["key"]=123
+#print dictionary
+list=['key','some more stuff','things and such']
+list.append('another stuff')
+#print list[3]
+
+for item in list:
+	print item
+	data=item.split('u')
+	print data
